9112aeb810fc2519ed2d66294ddb0cbef1e08b7a
doctea <doctea@gmail.com>
2022-05-22 20:21:58 +0100

    fixed encoder pinout ..

--- include/Config.h
+++ include/Config.h
@@ -5,7 +5,7 @@
 #define ENABLE_SCREEN
 #define TFT_SSOLED
 
-#define PIN_ENCODER_BUTTON  PIN_PC7
+#define PIN_ENCODER_BUTTON  PIN_PC5
 #define ENCODER_KNOB_R      PIN_PC6
-#define ENCODER_KNOB_L      PIN_PC5    
+#define ENCODER_KNOB_L      PIN_PC7    
 #define PIN_BUTTON_A        PIN_ENCODER_BUTTON
